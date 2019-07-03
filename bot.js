82cc6eacf7ef289dbff96a52772bec51bf312bdb
MotazMahmoud <50845789+MotazMahmoud@users.noreply.github.com>
2019-07-03 22:16:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,34 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "1";
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);  
+  console.log(Logged in as ${client.user.tag}!);
 });
 
 client.on('ready', async() => {
-var server = "596050841463423005"; // ايدي السررفر
-var channel = "596050841463423008";//ايدي الروم
+var server = "513033392468787241"; // ايدي السررفر
+var channel = "513034864296067073";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send(',Hi Btl And Meto Spam Anta men ??? ihihihihihihihidjfaagshfasfgasfhgjasgfhgasfgshagfhgasdfafsfghasghfhihihihihihihihihihihihihihihihhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhiasdghgasfgasgfvbnasvbfasvgfhgashgfhgashfgasghasjgskanfjnaskgnjhasgfhhaskfjjkasgshajfhnklsamlcvnjhasvbfgasgfhjaskkflsjakghsjkahoithuiashjfnasklnfgjkashgjasg,mas,gnjkasbhfghaskgjklasjhghauisghjakshgjhdsajkghakgjkljdklasghjkahsgkaskghsiohgioahgjklhasjhgjkasghghghghghghghghghghghghghghghghghghghi  ,
-, Hi Btl And Meto Spam Anta men ??? ihihihihihihihid')
+    client.guilds.get(server).channels.get(channel).send('Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , ')
     },305);
 })
 
-client.login(process.env.TOKEN);
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|`ADMINISTRATOR`ليس لديك صلاحيات`');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
  
+client.login(process.env.BOT_TOKEN);
